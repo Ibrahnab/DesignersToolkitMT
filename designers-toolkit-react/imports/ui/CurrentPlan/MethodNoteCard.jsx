880701e635d266649d99cd666311d751d5952b54
Ibrahnab <ibrahim.naboulsi97@gmail.com>
2022-04-14 01:42:32 +0200

    added scrollbar to overview notes + styled it

--- designers-toolkit-react/imports/ui/CurrentPlan/MethodNoteCard.jsx
+++ designers-toolkit-react/imports/ui/CurrentPlan/MethodNoteCard.jsx
@@ -7,11 +7,13 @@ import Col from 'react-bootstrap/Col';
 const MethodNoteCard = ({methodData, methodNote}) => {
     return(
         <div className="notes-box">
-            <Row>
-                <Col><img className="note-img" src={methodData.image}></img></Col>
+            <Row className="justify-content-md-center">
+                <Col className="d-flex justify-content-md-center ml-0">
+                    <img className="note-img" src={methodData.image}></img>
+                </Col>
             </Row>
             <Row>
-                <Col><p className="note-header">{methodData.name}</p></Col>
+                <Col><p className="note-header">{methodData.name}</p></Col> 
             </Row>
             <Row md="auto">
                 <Col><p className="note-text">{methodNote}</p></Col>
